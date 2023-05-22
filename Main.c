b567e9a831596f2196b2aa8558398ac5cfd753e1
Meldig23 <119503904+Meldig23@users.noreply.github.com>
2023-05-23 00:27:16 +0530

    Update Main.c
    We added tokens for all players and made them in form of structure so as to be able to move them further.

--- Main.c
+++ Main.c
@@ -1,4 +1,16 @@
 #include <SDL.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct {
+    int x;
+    int y;
+} Token;
+
+typedef struct {
+    Token tokens[4];
+    
+} Player;
 
 void drawRectangle(SDL_Renderer* renderer, int x, int y, int width, int height)
 {
@@ -10,11 +22,11 @@ void drawCircle(SDL_Renderer* renderer, int centerX, int centerY)
 {
     int radius = 13;
 
-    for (int y = -radius; y <= radius; ++y) 
+    for (int y = -radius; y <= radius; ++y)
     {
-        for (int x = -radius; x <= radius; ++x) 
+        for (int x = -radius; x <= radius; ++x)
         {
-            if (x * x + y * y <= radius * radius) 
+            if (x * x + y * y <= radius * radius)
             {
                 SDL_RenderDrawPoint(renderer, centerX + x, centerY + y);
             }
@@ -22,25 +34,27 @@ void drawCircle(SDL_Renderer* renderer, int centerX, int centerY)
     }
 }
 
-void renderBoard(SDL_Renderer* renderer) 
-{
-     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);//Black background
+void renderBoard(SDL_Renderer* renderer, Player players[4])
+{   
     
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);//Black background
+
     SDL_RenderClear(renderer);
     //entry line red
-    SDL_SetRenderDrawColor(renderer, 128, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 255, 12, 12, 255);
     drawRectangle(renderer, 32, 192, 32, 32);//single box
     drawRectangle(renderer, 32, 224, 160, 32);//long box
     //entry line blue
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 50, 10, 255, 255);
     drawRectangle(renderer, 224, 32, 32, 160);//long box
     drawRectangle(renderer, 256, 32, 32, 32);//single box
     //entry line yellow
-    SDL_SetRenderDrawColor(renderer, 255, 102, 19, 255);
+    SDL_SetRenderDrawColor(renderer, 255, 224, 19, 255);
     drawRectangle(renderer, 288, 224, 160, 32);//long box
     drawRectangle(renderer, 416, 256, 32, 32);//single box
     //entry line green
-    SDL_SetRenderDrawColor(renderer, 0, 51, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 50, 255, 10, 255);
     drawRectangle(renderer, 224, 288, 32, 160);//long line
     drawRectangle(renderer, 192, 416, 32, 32);//single box
 
@@ -73,36 +87,36 @@ void renderBoard(SDL_Renderer* renderer)
     SDL_RenderDrawLine(renderer, 192, 288, 288, 192);
 
     //Red Squares
-    SDL_SetRenderDrawColor(renderer, 128, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 255, 12, 12, 255);
     drawRectangle(renderer, 32, 32, 48, 48);
     drawRectangle(renderer, 32, 112, 48, 48);
     drawRectangle(renderer, 112, 112, 48, 48);
     drawRectangle(renderer, 112, 32, 48, 48);
-  
+
     //Blue Squares
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 50, 10, 255, 255);
     drawRectangle(renderer, 320, 32, 48, 48);
     drawRectangle(renderer, 400, 32, 48, 48);
     drawRectangle(renderer, 320, 112, 48, 48);
     drawRectangle(renderer, 400, 112, 48, 48);
-    
-    //Green rectangle   
-    SDL_SetRenderDrawColor(renderer, 0, 51, 0, 255);
+
+    //Green squares   
+    SDL_SetRenderDrawColor(renderer, 50, 255, 10, 255);
     drawRectangle(renderer, 32, 320, 48, 48);
     drawRectangle(renderer, 32, 400, 48, 48);
     drawRectangle(renderer, 112, 320, 48, 48);
     drawRectangle(renderer, 112, 400, 48, 48);
-    
-    //Yellow rectangle
-    SDL_SetRenderDrawColor(renderer, 255, 102, 19, 255);
+
+    //Yellow squares
+    SDL_SetRenderDrawColor(renderer, 255, 224, 19, 255);
     drawRectangle(renderer, 320, 320, 48, 48);
     drawRectangle(renderer, 320, 400, 48, 48);
     drawRectangle(renderer, 400, 400, 48, 48);
     drawRectangle(renderer, 400, 320, 48, 48);
-    
+
     //Triangles
     //Red Triangle
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 255, 12, 12, 255);
     for (int x = 0; x <= 640; x++) {
         for (int y = 0; y <= 480; y++) {
             if (x > 192 && y > x && (x + y) < 480)
@@ -111,7 +125,7 @@ void renderBoard(SDL_Renderer* renderer)
     }
 
     //Blue triangle
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 50, 10, 255, 255);
     for (int x = 0; x <= 640; x++) {
         for (int y = 0; y <= 480; y++) {
             if (y > 192 && x > y && (x + y) < 480)
@@ -119,7 +133,7 @@ void renderBoard(SDL_Renderer* renderer)
         }
     }
     //Yellow triangle
-    SDL_SetRenderDrawColor(renderer, 255,224 ,19, 255);
+    SDL_SetRenderDrawColor(renderer, 255, 224, 19, 255);
     for (int x = 0; x <= 640; x++) {
         for (int y = 0; y <= 480; y++) {
             if (x < 288 && y < x && (x + y) > 480)
@@ -134,48 +148,91 @@ void renderBoard(SDL_Renderer* renderer)
                 SDL_RenderDrawPoint(renderer, x, y);
         }
     }
-    
-     // Red token
-    SDL_SetRenderDrawColor(renderer, 160, 0, 20, 255);
-    
-    drawCircle(renderer, 32 + 48 / 2,32 + 48 / 2);
-    drawCircle(renderer, 32 + 48 / 2, 112 + 48 / 2);
-    drawCircle(renderer, 112 + 48 / 2, 32 + 48 / 2);
-    drawCircle(renderer, 112 + 48 / 2, 112 + 48 / 2);
 
+    // Red token
+    
+    for (int i = 0; i < 4; i++) {
+        if (i == 0) SDL_SetRenderDrawColor(renderer, 160, 0, 20, 255);
+        if (i==1) SDL_SetRenderDrawColor(renderer, 50, 10, 150, 255);
+        if (i==2) SDL_SetRenderDrawColor(renderer, 178, 255, 102, 255);
+        if (i==3) SDL_SetRenderDrawColor(renderer, 255, 255, 153, 255);
+        for (int j = 0; j < 4; j++) {
+            drawCircle(renderer, players[i].tokens[j].x, players[i].tokens[j].y);
+        }
+    }
+    
+    
+
+
+    
 
     SDL_RenderPresent(renderer);
 }
 
-int main()
+int rolldice() {
+    return rand() % 6 + 1;
+}
+
+
+
+int main(int* argc,char * argv[])
 {
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
+
+
 
     SDL_Init(SDL_INIT_VIDEO);
 
     window = SDL_CreateWindow("Ludo Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_Event event;
-    int quit = 0;
+    
+    Player players[4];
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            players[i].tokens[j].x = 56+!((i+1) % 2) * 288 + 80 * (!((j+1) % 2));
+            players[i].tokens[j].y = 56+288*((i+1)>2)+ 80*((j+1)>2); 
+        }
+    }
+    
 
-    while (!quit) 
+    
+    SDL_Event event = { 0 };
+    int quit = 0;
+    int dicenumber=0;
+    while (!quit)
     {
+
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_QUIT)
             {
                 quit = 1;
             }
+            else if (event.type == SDL_MOUSEBUTTONDOWN) {
+                if (event.button.button == SDL_BUTTON_LEFT) {
+                    int x = event.button.x;
+                    int y = event.button.y;
+
+                }
+
+
+
+
+            }
         }
 
-        renderBoard(renderer);
+        renderBoard(renderer, players);
     }
+
+    
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
+    
     return 0;
+
 }
+
